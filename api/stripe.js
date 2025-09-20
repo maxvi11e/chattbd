186f49f10759a0950f33b85293bd4cddc9913e64
Max Lewis <max@Maxs-MacBook-Pro-2.local>
2025-09-20 13:56:58 -0700

    Temporarily set starter plan to /bin/bash.01 for webhook testing

--- api/stripe.js
+++ api/stripe.js
@@ -381,7 +381,7 @@ function getPlanDetails(planId) {
     'starter': {
       name: 'Starter Plan',
       description: 'Great for small projects and personal use',
-      price_cents: 500,
+      price_cents: 1,
       billing_interval: 'one_time',
       bot_limit: 4
     },
